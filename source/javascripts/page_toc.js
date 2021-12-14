305dcfaf853046b5158d3b137bd997a55774cd66
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2021-12-14 15:50:25 +0000

    javascript

--- source/javascripts/page_toc.js
+++ source/javascripts/page_toc.js
@@ -1,22 +1,22 @@
 window.onload = function toc() {
-    const h2_list = document.querySelectorAll('h2.anchored-heading')
+    const h2List = document.querySelectorAll('h2.anchored-heading')
 
     const ul = document.createElement('ul');
     ul.setAttribute('style', 'list-style-type:none');
     ul.setAttribute('class', 'toc__list');
 
-    h2_list.forEach(function (h) {
-        var a = document.createElement('a');
-        var a_text = document.createTextNode(h.innerText);
-        a.append(a_text);
+    h2List.forEach(function (h) {
+        const a = document.createElement('a');
+        const aText = document.createTextNode(h.innerText);
+        a.append(aText);
         a.setAttribute('href', '#'+h.id);
-        var li = document.createElement('li');
+        const li = document.createElement('li');
         li.append(a);
         ul.append(li);
     })
 
-    const page_toc = document.getElementById('page_toc');
-    page_toc.setAttribute('style', 'border-style:solid;border-width:1px;width:fit-content;margin-bottom:20px')
+    const pageTOC = document.getElementById('page_toc');
+    pageTOC.setAttribute('style', 'border-style:solid;border-width:1px;width:fit-content;margin-bottom:20px')
 
-    page_toc.append(ul)
+    pageTOC.append(ul)
 }
